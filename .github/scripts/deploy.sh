6acce9d13762468dd4178cda3e685424bb97181f
dbssuppport <164154832+dbssuppport@users.noreply.github.com>
2024-03-29 23:38:57 +0100

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -5,9 +5,11 @@ set -e
 KEY_FILE=$1
 GOOGLE_PROJECT=$2
 LIBRARIES_BUCKET=$3
-ARTIFACT=$4
+ARTIFACT_CLI=$4
+ARTIFACT_SERVICE=$5
 
 echo ${KEY_FILE} | base64 -d > client-secret.json
 gcloud auth login --cred-file=client-secret.json 
 gcloud config set project ${GOOGLE_PROJECT}
-gcloud storage cp ${ARTIFACT} gs://${LIBRARIES_BUCKET}/${ARTIFACT}
+gcloud storage cp ${ARTIFACT_CLI} gs://${LIBRARIES_BUCKET}/${ARTIFACT_CLI}
+gcloud storage cp ${ARTIFACT_SERVICE} gs://${LIBRARIES_BUCKET}/${ARTIFACT_SERVICE}
