36526985006419738f2fb7ea6e3e6a68b0ebe451
dbssuppport <164154832+dbssuppport@users.noreply.github.com>
2024-03-29 23:11:36 +0100

    Update and rename gcp.sh to deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -4,9 +4,10 @@ set -e
 
 KEY_FILE=$1
 GOOGLE_PROJECT=$2
+LIBRARIES_BUCKET=$3
+ARTIFACT=$4
 
 echo ${KEY_FILE} | base64 -d > client-secret.json
 gcloud auth login --cred-file=client-secret.json 
 gcloud config set project ${GOOGLE_PROJECT}
-ls -al routine-finder-service/build/libs
-#gcloud storage cp spark/historical_load_currencies_exchange_rate.py gs://${LIBRARIES_BUCKET}/spark/historical-rates/historical_load_currencies_exchange_rate.py
+gcloud storage cp $ARTIFACT gs://${LIBRARIES_BUCKET}/$ARTIFACT
