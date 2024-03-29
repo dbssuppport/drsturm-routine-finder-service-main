9e4349accd7b9d0e9036da78df86c9eb465e2464
dbssuppport <164154832+dbssuppport@users.noreply.github.com>
2024-03-29 22:29:47 +0100

    Update deploy.sh

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -9,5 +9,5 @@ GOOGLE_PROJECT=$2
 echo "CURDIR=$CURDIR"
 echo "GOOGLE_PROJECT=$GOOGLE_PROJECT"
 
-docker run --name gcp_cli -v ${CURDIR}:/work_dir --env KEY_FILE=$KEY_FILE --env GOOGLE_PROJECT=$GOOGLE_PROJECT google/cloud-sdk:428.0.0-slim sh -c "cd /work_dir && echo ${KEY_FILE} | base64 -d > client-secret.json && gcloud auth login --cred-file=client-secret.json && gcloud config set project ${GOOGLE_PROJECT}"
+docker run --name gcp_cli -v ${CURDIR}:/work_dir --env KEY_FILE=$KEY_FILE --env GOOGLE_PROJECT=$GOOGLE_PROJECT google/cloud-sdk:428.0.0-slim sh -c "cd /work_dir && chmod +x .github/scripts/gcp.sh && ./.github/scripts/gcp.sh"
 docker rm --force gcp_cli
