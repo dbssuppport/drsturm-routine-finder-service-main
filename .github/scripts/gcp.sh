2aa55b2085d80d68015da32ca45cc4351b1b539b
dbssuppport <164154832+dbssuppport@users.noreply.github.com>
2024-03-29 22:36:18 +0100

    Create gcp.sh

---
+++ .github/scripts/gcp.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+set -e
+
+echo ${KEY_FILE} | base64 -d > client-secret.json
+gcloud auth login --cred-file=client-secret.json 
+gcloud config set project ${GOOGLE_PROJECT}
+ls -al routine-finder-service/build/libs
+#gcloud storage cp spark/historical_load_currencies_exchange_rate.py gs://${LIBRARIES_BUCKET}/spark/historical-rates/historical_load_currencies_exchange_rate.py
