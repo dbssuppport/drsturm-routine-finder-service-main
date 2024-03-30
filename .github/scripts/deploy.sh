0c7d85b20468180896baea518b9ca20c41c6dd46
dbssuppport <164154832+dbssuppport@users.noreply.github.com>
2024-03-30 16:04:47 +0100

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -5,11 +5,14 @@ set -e
 KEY_FILE=$1
 GOOGLE_PROJECT=$2
 LIBRARIES_BUCKET=$3
-ARTIFACT_CLI=$4
-ARTIFACT_SERVICE=$5
+ARTIFACT_CLI_PATH=$4
+ARTIFACT_SERVICE_PATH=$5
+
+ARTIFACT_CLI=$(echo $ARTIFACT_CLI_PATH | cut -d '/' -f4)
+ARTIFACT_SERVICE=$(echo $ARTIFACT_CLI_PATH | cut -d '/' -f4)
 
 echo ${KEY_FILE} | base64 -d > client-secret.json
 gcloud auth login --cred-file=client-secret.json 
 gcloud config set project ${GOOGLE_PROJECT}
-gcloud storage cp ${ARTIFACT_CLI} gs://${LIBRARIES_BUCKET}/routine-finder-cli/${ARTIFACT_CLI}
-gcloud storage cp ${ARTIFACT_SERVICE} gs://${LIBRARIES_BUCKET}/routine-finder-service/${ARTIFACT_SERVICE}
+gcloud storage cp ${ARTIFACT_CLI_PATH} gs://${LIBRARIES_BUCKET}/routine-finder-cli/${ARTIFACT_CLI}
+gcloud storage cp ${ARTIFACT_SERVICE_PATH} gs://${LIBRARIES_BUCKET}/routine-finder-service/${ARTIFACT_SERVICE}
