ed150c0842009161915ed996282970302096aaf8
dbssuppport <164154832+dbssuppport@users.noreply.github.com>
2024-04-10 20:30:15 +0200

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -8,6 +8,7 @@ LIBRARIES_BUCKET=$3
 ARTIFACT_CLI_PATH=$4
 ARTIFACT_SERVICE_PATH=$5
 ENV=$6
+ROUTINE_FINDER_BUCKET=$7
 
 ARTIFACT_CLI=$(echo $ARTIFACT_CLI_PATH | cut -d '/' -f4)
 ARTIFACT_SERVICE=$(echo $ARTIFACT_CLI_PATH | cut -d '/' -f4)
@@ -18,3 +19,4 @@ gcloud config set project ${GOOGLE_PROJECT}
 gcloud storage cp ${ARTIFACT_CLI_PATH} gs://${LIBRARIES_BUCKET}/routine-finder-cli/${ARTIFACT_CLI}
 gcloud storage cp ${ARTIFACT_SERVICE_PATH} gs://${LIBRARIES_BUCKET}/routine-finder-service/${ARTIFACT_SERVICE}
 gcloud app deploy ${ARTIFACT_SERVICE_PATH} --appyaml=".github/scripts/app_config/${ENV}.yaml" --project="${GOOGLE_PROJECT}"
+java -jar "${ARTIFACT_CLI_PATH}" import "mindmup/import.json" "${ROUTINE_FINDER_BUCKET}" false
